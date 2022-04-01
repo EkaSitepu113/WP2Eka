a8e9feb2f92c8310aaaef8447a3ad0a6022d05ab
Eka Putra Sitepu <68458911+EkaSitepu113@users.noreply.github.com>
2022-04-02 01:33:42 +0700

    Add files via upload

---
+++ v_about.php
@@ -0,0 +1,30 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Eka Putra</li>
+        <li>Nama Belakang : Sitepu</li>
+    </ul>
+    <br>
+        <h4>Alamat</h4>
+        <ul type="none">
+        <li> Jalan Banjarmasin 2</li>
+    </ul>
+ 
+        <h4>Tempat Lahir</h4>
+        <ul type="none">
+        <li>Tangerang</li>
+    </ul>
+
+        <h4>Olah Raga Favorit</h4>
+        <ul type="square">
+        <li>Badmintoon</li>
+        <li>Futsal</li>
+    </ul>
+</section>
+
+
+
+
+
+
